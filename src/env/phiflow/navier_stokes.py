b9e97891fc9b9873fcdbaa02c2ce3fa2999869f9
ajrox <ajrocks0509@gmail.com>
2022-07-29 20:33:31 +0200

    simple navier stokes incomplete

--- src/env/phiflow/navier_stokes.py
+++ src/env/phiflow/navier_stokes.py
@@ -1,26 +1,39 @@
-from phi.physics import diffuse, advect, fluid
 from phi.physics._effect import effect_applied
 from phi.physics._physics import StateDependency, Physics
+from phi.flow import *  # minimal dependencies
+# from phi.torch.flow import *
 
 
 class NavierStokes(Physics):
 
-    def __init__(self, viscosity=0.1, buoyancy_factor=1.0):
-        Physics.__init__(self, [StateDependency('effects', 'velocity_effect', blocking=True)])
-        self.viscosity = viscosity
-        self.buoyancy_factor = buoyancy_factor
+    def __init__(self):
+        Physics.__init__(self, dependencies=[
+            StateDependency('pressure', 'pressure'),
+            StateDependency('boundary_mask', 'boundary_mask'),
+            StateDependency('obstacles', 'obstacle'),
+            StateDependency('velocity_effects', 'velocity_effect', blocking=True),
+        ])
 
-    def step(self, v, dt=1.0, effects=()):
-        return NavierStokes.step_forward(velocity=v, viscosity=self.viscosity,
-                                         dt=dt, effects=effects)
+    def step(self, v, dt=1.0, boundary_mask=None,
+             pressure=None, obstacles=(), velocity_effects=()):
+        v = advect.semi_lagrangian(v, v, dt=dt)  # + dt * buoyancy_force
+        if boundary_mask is not None:
+            v = v * (1 - boundary_mask) + boundary_mask * (2.0, 0)
+        for effect in velocity_effects:
+            v = effect_applied(effect, v, dt)
+        v, pressure = fluid.make_incompressible(v, obstacles, Solve('auto', 1e-5, 0, x0=pressure))
+        vorticity = field.curl(v)
+        return v, pressure, vorticity
 
-    @staticmethod
-    def step_forward(velocity, viscosity, dt=1.0, effects=()):
-        # smoke = advect.semi_lagrangian(smoke, velocity, dt) + INFLOW
-        # smoke = advect.semi_lagrangian(smoke, velocity, dt)
-        # buoyancy_force = (smoke * (0, self.buoyancy_factor)).at(velocity)  # resamples smoke to velocity sample points
-        velocity = advect.semi_lagrangian(velocity, velocity, dt)   # + dt * buoyancy_force
-        velocity = diffuse.explicit(velocity, viscosity, dt)
-        velocity, pressure = fluid.make_incompressible(velocity)
-        return velocity, pressure
-        # return velocity, smoke, pressure
+# For Example:
+# SPEED = vis.control(2.)
+# CYLINDER1 = Obstacle(geom.infinite_cylinder(x=15, y=32, radius=5, inf_dim=None))
+# CYLINDER2 = Obstacle(geom.infinite_cylinder(x=15, y=64, radius=5, inf_dim=None))
+#
+# velocity = StaggeredGrid((SPEED, 0), extrapolation.BOUNDARY, x=128, y=128, bounds=Box(x=128, y=100))
+# BOUNDARY_MASK = StaggeredGrid(Box(x=(-INF, 0.5), y=None),
+#           velocity.extrapolation, velocity.bounds, velocity.resolution)
+# pressure = None
+#
+# ns = NavierStokes()
+# ns.step(velocity=velocity, pressure=pressure)
---
+++ tests/simple_ks_simulation.py
@@ -0,0 +1,54 @@
+import math
+import sys
+
+from phi.math import pi
+from copy import deepcopy
+
+from matplotlib import pyplot as plt
+from phi.flow import *
+from src.env.phiflow.burgers import Burgers
+from src.env.phiflow.ks import KuramotoSivashinsky
+
+N = 32
+domain_dict = dict(x=N, extrapolation=extrapolation.PERIODIC)
+step_count = 1
+dt = 0.01
+
+
+def officialGaussianClash(x):
+    batch_size = 32
+    leftloc = np.random.uniform(0.2, 0.4)
+    leftamp = np.random.uniform(0, 3)
+    leftsig = np.random.uniform(0.05, 0.15)
+    rightloc = np.random.uniform(0.6, 0.8)
+    rightamp = np.random.uniform(-3, 0)
+    rightsig = np.random.uniform(0.05, 0.15)
+    left = leftamp * math.exp(-0.5 * (x.vector[0] - leftloc) ** 2 / leftsig ** 2)
+    right = rightamp * math.exp(-0.5 * (x.vector[0] - rightloc) ** 2 / rightsig ** 2)
+    result = left + right
+    return result
+
+
+def officialGaussianForce(x):
+    batch_size = 32
+    loc = np.random.uniform(0.4, 0.6, batch_size)
+    amp = np.random.uniform(-0.05, 0.05, batch_size) * 32
+    sig = np.random.uniform(0.1, 0.4, batch_size)
+    result = tensor(amp, x.shape[0]) * math.exp(
+        -0.5 * (x.x.tensor - tensor(loc, x.shape[0])) ** 2 / tensor(sig, x.shape[0]) ** 2)
+    return result
+
+
+def burgers_rkstiff_function(x):
+    u0 = math.exp(-10 * math.sin(x / 2) ** 2)
+    return u0
+
+
+u = CenteredGrid(lambda x: math.sin(x), **domain_dict)
+
+physics = KuramotoSivashinsky()
+
+for _ in view(play=False, namespace=globals()).range():
+# for _ in range(5):
+    u = physics.step(u, dt=dt)
+    # vis.show(u)
