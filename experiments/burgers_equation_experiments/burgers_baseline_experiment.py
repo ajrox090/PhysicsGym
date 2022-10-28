a2965ec8374b8a906d519a4cf67b9c350653f73f
ajrox <ajrocks0509@gmail.com>
2022-10-28 11:39:04 +0200

    baseline solution for burgers equation control experiment

--- experiments/burgers_equation_experiments/burgers_baseline_experiment.py
+++ experiments/burgers_equation_experiments/burgers_baseline_experiment.py
@@ -1,4 +1,6 @@
 from phi.flow import *
+
+from src.agent.BaselineAgent import BaselineAgent
 from src.env.BurgersPhysicsGym import BurgersPhysicsGym
 from src.util import plotGrid
 
@@ -7,7 +9,7 @@ dt = 0.01
 dx = 0.25
 domain = 4
 step_count = 100
-viscosity = 0.003
+viscosity = 0.03
 N = int(domain / dx)
 
 domain_dict = dict(x=int(domain / dx), bounds=Box[0:1],
@@ -18,23 +20,27 @@ env = BurgersPhysicsGym(domain=domain, dx=dx, domain_dict=domain_dict,
                         xlim=0, ylim=2.0)
 
 observation = env.reset()
-# env.enable_rendering()
-results = []
-labels = []
+statesList = []
+statelabels = []
+actionList = []
+actionLabels = []
+agent = BaselineAgent(env=env, u_min=-1.0, u_max=1.0)
 for _ in range(step_count):
-    if _ < 0:
-        a = -1.0
-    else:
-        a = 0.0
-    actions = np.array([a])
+    actions = agent.predict(observation)
     # if _ % ((step_count - 1) // 5) == 0:
     #     env.enable_rendering()
-    if _ % ((step_count - 1) // 3) == 0:
-        results.append(observation)
-        labels.append(f'state at t={env.step_idx/100}')
     observation, reward, done, info = env.step(actions)
+    # if _ % ((step_count - 1) // 3) == 0:
+    if _ in [1, 15, 30, 60, 99]:
+        statesList.append(observation)
+        statelabels.append(f't={env.step_idx/100}')
+        actionList.append(env.forces.field.data.numpy("vector,x")[0])
+        actionLabels.append(f't={env.step_idx/100}')
     # if env._render:
     #     env.disable_rendering()
 
 # env.disable_rendering()
-plotGrid(listU=results, domain=domain, dx=dx, label=labels)
+plotGrid(listU=statesList, domain=domain, dx=dx, label=statelabels,
+         ylim_min=-2.0, ylim_max=2.0)
+plotGrid(listU=actionList, domain=domain, dx=dx, label=actionLabels,
+         xlim_max=1.5, ylim_max=1.0, ylim_min=-1.0)
--- src/agent/BaselineAgent.py
+++ src/agent/BaselineAgent.py
@@ -6,63 +6,34 @@ import phi
 from phi import math
 from phi.field import CenteredGrid
 
-from stable_baselines3.ppo import MlpPolicy
-from stable_baselines3.common.base_class import BaseAlgorithm
-from stable_baselines3.common.type_aliases import MaybeCallback, GymEnv
-
+from src.agent.PhysicsAgent import PhysicsAgent
 from src.env.PhysicsGym import PhysicsGym
 
 
-class MPCAgent(BaseAlgorithm):
+class BaselineAgent(PhysicsAgent):
 
-    def __init__(self, env: PhysicsGym,
-                 u_max: float = 1.0,  # max control input
-                 u_min: float = -1.0,  # min control input
-                 ):
-        super().__init__(policy=MlpPolicy, env=env, learning_rate=1.0)
-        self.env = env
+    def __init__(self, env: PhysicsGym, u_max: float = 1.0, u_min: float = -1.0):
+        super().__init__(env=env)
+        self.u0 = np.array([0, 0])
+        self.bounds = tuple(zip([u_min for _ in range(2)], [u_max for _ in range(2)]))
 
-        self.u0 = np.zeros(1)
-
-        self.bounds = tuple([u_min, u_max])
-
-        self.ref_state = env.reference_state_np.flatten()
-        self.shape_nt_state = env.init_state.data.native("x,vector").shape[0]
-        self.shape_phi_state = env.init_state.shape
-
-    def predict(
-            self,
-            observation: np.ndarray,
-            state: Optional[np.ndarray] = None,
-            episode_start: Optional[np.ndarray] = None,
-            deterministic: bool = False,
-    ) -> Tuple[np.ndarray, Optional[Tuple[np.ndarray, ...]]]:
-        res = minimize(lambda u: self.cost_function(u, observation),
-                       self.u0, method='SLSQP', bounds=self.bounds,
+    def predict(self, observation: np.ndarray, state: Optional[np.ndarray] = None,
+                episode_start: Optional[np.ndarray] = None, deterministic: bool = False) \
+            -> Tuple[np.ndarray, Optional[Tuple[np.ndarray, ...]]]:
+        res = minimize(lambda u: self.cost_function(u, observation), self.u0, method='SLSQP', bounds=self.bounds,
                        options={'eps': 1e-1})
-
         if not res.success:
             raise Warning("Failed to find minimum")
             print(f'{res.x} : {res.fun}')
+
         return [res.x[0]]
 
     def cost_function(self, u_, y0_):
-        y0_ = CenteredGrid(phi.math.tensor(y0_, self.shape_phi_state), **self.env.domain_dict)
-
-        y0_ = self.env.step_physics(in_state=y0_,
-                                    effects=(self.env._scalar_action_to_forces([u_[0]]),))
+        y0_ = self.env.step_physics(in_state=CenteredGrid(phi.math.tensor(y0_, self.shape_phi_state),
+                                                          **self.env.domain_dict),
+                                    effects=(self.env.scalar_action_to_forces([u_[0]]),))
 
         loss = np.sum(((y0_.data.native("vector,x")[0] - self.env.reference_state_np) ** 2) / self.env.N, axis=-1)
         loss = np.sum(loss, axis=0)
 
         return loss
-
-    def _setup_model(self) -> None:
-        pass
-
-    def learn(self, total_timesteps: int, callback: MaybeCallback = None, log_interval: int = 100,
-              tb_log_name: str = "run", eval_env: Optional[GymEnv] = None, eval_freq: int = -1,
-              n_eval_episodes: int = 5, eval_log_path: Optional[str] = None,
-              reset_num_timesteps: bool = True) -> "BaseAlgorithm":
-        """ MPC is an optimization algorithm, it doesn't learn anything """
-        return self
